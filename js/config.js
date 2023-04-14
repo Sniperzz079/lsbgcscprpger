e09545a81d91379569ce33f5863559d4cf4b8e02
Sniperzz079 <115150209+Sniperzz079@users.noreply.github.com>
2023-04-14 17:57:58 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,54 @@
+// ignore
+var Config = {};
+
+/**
+ * What should the text in the center of the screen be?
+ * if empty it will fill in your Server Name
+ */
+Config.title = "//BGC SCP-RP [2.5] |SITE-99|//";
+
+/**
+ * Enable map text in the top left corner of the screen?
+ */
+Config.enableMap = true;
+
+/**
+ * Enable steamId text in the top right corner of the screen?
+ */
+Config.enableSteamID = true;
+
+/**
+ * Enable announcements?
+ */
+Config.enableAnnouncements = true;
+
+/**
+ * What messages do you want to show up?
+ * only works if enableAnnouncements = true
+ */
+Config.announceMessages = [
+  "//Geniße den aufenthalt mit Uns!//",
+  "//Der Loadingscreen wurde erstellt von: Mr.Shadow/Sniperzz//"
+];
+
+/**
+ * How many miliseconds for each announcement?
+ * only works if enableAnnouncements = true
+ */
+Config.announcementLength = 3000;
+
+/**
+ * Image Filename
+ * DROP IMAGE IN "images" FOLDER
+ */
+Config.backgroundImage = "";
+
+/**
+ * Enable debug messages?
+ */
+Config.enableDebug = false;
+
+/**
+ * Music play
+ */
+Config.musicplay = true;
